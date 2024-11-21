38482db5aa38974ff18de903a63e7068b536060f
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-21 11:46:06 -0600

    Update app.js

--- t2s-courses-backend/src/app.js
+++ t2s-courses-backend/src/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const coursesRouter = require('./routes/courses');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/courses', coursesRouter);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
