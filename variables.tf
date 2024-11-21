5e1766906ed28924e7bd13a402a6b88313978752
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-21 10:38:52 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "environment" {
+  description = "Environment name (dev, stage, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
